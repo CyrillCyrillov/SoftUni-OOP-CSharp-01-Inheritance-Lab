1399dd5f39f3c59234ca7e4f098ed8158ab566bc
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-26 19:33:34 +0200

    Add files via upload

---
+++ Task05_Stack_of_Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings testStack = new StackOfStrings();
+
+            testStack.IsEmpty();
+
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => n).ToArray();
+
+            foreach (string item in input)
+            {
+                testStack.AddRange(item);
+            }
+
+            testStack.IsEmpty();
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task05_Stack_of_Strings/StackOfStrings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if(this.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Not Empty!");
+                return false;
+            }
+        }
+
+        public Stack<string> AddRange(string newString)
+        {
+            this.Push(newString);
+            return this;
+        }
+
+
+
+    }
+}
