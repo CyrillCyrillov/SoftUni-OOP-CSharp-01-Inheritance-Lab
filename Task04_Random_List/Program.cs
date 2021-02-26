81035a0ff2611970c5ce11576119d6316c027063
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-26 18:44:36 +0200

    Add files via upload

---
+++ Task04_Random_List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList testList = new RandomList();
+            testList.Add("Kiril");
+            testList.Add("Petar");
+            testList.Add("Ivan");
+            testList.Add("Lili");
+            testList.Add("Sophy");
+            testList.Add("Alex");
+            testList.Add("John");
+            testList.Add("Perla");
+            testList.Add("Jane");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                testList.RandomString();
+            }
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task04_Random_List/RandomList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random randomElement;
+
+        /*
+        public RandomList()
+        {
+            randomElement = new Random();
+        }
+        */
+
+
+        public void RandomString()
+        {
+            randomElement = new Random();
+            int indexToRemove = randomElement.Next(0, this.Count);
+            Console.WriteLine($"Random removed: {this[indexToRemove]}");
+            this.RemoveAt(indexToRemove);
+        }
+
+    }
+}
